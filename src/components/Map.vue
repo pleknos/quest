e6acf60db9e0f34c740f89e13a713761ea597f78
ple <pleknosthe@gmail.com>
2021-07-06 20:23:33 +0300

    add watch position

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,18 +1,40 @@
 <template>
-  <div id="map"></div>
+  <div id="map" @click="show"></div>
 </template>
 
 <script>
 import * as TwoGis from '2gis-maps';
 
 export default {
+  data() {
+    return {
+      map: null,
+      showPlayer: false,
+      playerCoords: [ 55.86, 38.45 ],
+      playerMarker: null,
+    };
+  },
   mounted() {
-    const map = DG.map('map', {
-      'center': [ 54.98, 82.89 ],
+    this.map = TwoGis.map('map', {
+      'center': [ 55.86, 38.45 ],
       'zoom': 13,
     });
 
-    DG.control.location({ drawCircle: true, follow: true }).addTo(map);
+    navigator.geolocation.watchPosition(position => {
+      const { latitude, longitude } = position.coords;
+      this.playerCoords = [ latitude, longitude ];
+
+      if (this.showPlayer) {
+        this.playerMarker.setLatLng([ latitude, longitude ]);
+      }
+      if (!this.showPlayer) this.showPlayer = true;
+    }, () => {}, { enableHighAccuracy: true });
+  },
+  watch: {
+    showPlayer() {
+      this.playerMarker = TwoGis.marker(this.playerCoords);
+      this.playerMarker.addTo(this.map);
+    },
   },
 };
 </script>
