97e597e9223c76298fa72ed21e4fd84562d486d4
ple <pleknosthe@gmail.com>
2021-07-08 21:01:33 +0300

    add targets

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="map"></div>
+  <div class="questions">
+    <form class="question" v-for="target of activeTargets" @submit.prevent="sendAnswer(target)">
+      <div class="input-group">
+        <label :for="target.id">{{ target.question }}</label>
+        <input :id="target.id" type="text" name="answer" required/>
+      </div>
+      <div class="input-group button-group">
+        <button type="submit" class="btn btn-green">Отправить ответ</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -12,6 +23,42 @@ export default {
       showPlayer: false,
       playerCoords: [ 55.86, 38.45 ],
       playerMarker: null,
+      targets: [
+        {
+          id: 1,
+          coords: [ 55.876423, 38.468773 ],
+          question: 'Вопрос 1',
+          answerType: 'text',
+          name: 'Точка 1',
+          active: false,
+        },
+        {
+          id: 2,
+          coords: [ 55.878353, 38.470219 ],
+          question: 'Вопрос 2',
+          answerType: 'text',
+          name: 'Точка 2',
+          active: false,
+        },
+        {
+          id: 3,
+          coords: [ 55.877447, 38.465088 ],
+          question: 'Вопрос 3',
+          answerType: 'text',
+          name: 'Точка 3',
+          active: false,
+        },
+      ],
+      activeTargets: [
+        { id: 1, coords: [ 55.876423, 38.468773 ], question: 'Как какать?', answerType: 'text', name: 'Точка 1' },
+        {
+          id: 2,
+          coords: [ 55.877447, 38.465088 ],
+          question: 'Ты зачем это сделал?',
+          answerType: 'text',
+          name: 'Точка 3',
+        },
+      ],
     };
   },
   mounted() {
@@ -20,9 +67,34 @@ export default {
       'zoom': 13,
     });
 
+    for (const target of this.targets) {
+      const marker = TwoGis.marker(target.coords, {
+        popupAnchor: [ 0, 0 ],
+        icon: TwoGis.divIcon({
+          className: 'target-marker',
+          iconSize: [ 20, 20 ],
+          riseOnHover: true,
+        }),
+      });
+
+      marker.bindPopup(`<p style="text-align: center">${ target.name }</p>`).openPopup();
+
+      marker.addTo(this.map);
+    }
+
     navigator.geolocation.watchPosition(position => {
-      const { latitude, longitude } = position.coords;
+      const { latitude, longitude, accuracy } = position.coords;
       this.playerCoords = [ latitude, longitude ];
+
+      for (const target of this.targets) {
+        if (latitude - 0.0001 < target.coords[0] && latitude + 0.0001 > target.coords[0] &&
+            longitude - 0.0001 < target.coords[1] && longitude + 0.0001 > target.coords[1]) {
+          if (!target.active) {
+            this.activeTargets.push(target);
+          }
+          target.active = true;
+        }
+      }
 
       if (this.showPlayer) {
         this.playerMarker.setLatLng([ latitude, longitude ]);
@@ -41,13 +113,19 @@ export default {
       }
 
     }, () => {}, { enableHighAccuracy: true });
+
+  },
+  methods: {
+    sendAnswer(target) {
+      this.activeTargets = this.activeTargets.filter(activeTarget => {
+        return target.id !== activeTarget.id;
+      });
+      alert('Неправильно!');
+    },
   },
 };
 </script>
 
 <style scoped>
-#map {
-  width: 100vw;
-  height: 100vh;
-}
+@import '@/assets/css/components/map.css';
 </style>
--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -117,6 +117,7 @@ export default {
       this.card++;
     },
     prevCard() {
+
       this.card--;
     },
     setValue(field, event) {
