0d04c68d22726868adfb00887feaafda4272ba7c
ple <pleknos@yandex.ru>
2021-07-21 08:08:02 +0000

    pl

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -3,8 +3,10 @@
     <router-link to="/register" class="btn" v-if="!user.token">Регистрация</router-link>
     <router-link to="/login" class="btn" v-if="!user.token && isStarted">Войти</router-link>
     <router-link to="/map" class="btn" v-if="user.token && isStarted">Карта</router-link>
-    <h2 v-if="user.token && !isStarted">Вы успешно зарегистрировались!</h2>
-    <p>Теперь вы можете расслабиться и ожидать начала фестиваля</p>
+    <div v-if="user.token && !isStarted">
+      <h2>Вы успешно зарегистрировались!</h2>
+      <p>Теперь вы можете расслабиться и ожидать начала фестиваля</p>
+    </div>
   </div>
 
 </template>
