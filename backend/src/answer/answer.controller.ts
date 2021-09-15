ee7e5abfff0db0717572e181c27a1f22885d68b2
ple <pleknos@yandex.ru>
2021-09-15 13:28:59 +0000

    fix

--- backend/src/answer/answer.controller.ts
+++ backend/src/answer/answer.controller.ts
@@ -21,7 +21,8 @@ export class AnswerController {
 
   @Get('winners')
   getWinners(@Req() request) {
-    if (request.user.id !== 40 || request.user.id !== 24) throw new ForbiddenException();
+    console.log(request.user)
+    if (request.user.id !== 40 && request.user.id !== 24) throw new ForbiddenException();
     return this.answerService.getWinners();
   }
 }
