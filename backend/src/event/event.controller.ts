75701d50678ffd5d47b559bf9d08ba621cb08f15
ple <pleknosthe@gmail.com>
2021-09-17 23:42:46 +0300

    fix

--- backend/src/event/event.controller.ts
+++ backend/src/event/event.controller.ts
@@ -11,7 +11,7 @@ export class EventController {
   @Get()
   @Public()
   async get(@Req() request) {
-    if (request.user.id === 40 || request.user.id === 154 || request.user.id === 24) return { started: true };
+    if (request.user?.id === 40 || request.user?.id === 154 || request.user?.id === 24) return { started: true };
 
     return {
       started: await this.eventService.isStarted(),
