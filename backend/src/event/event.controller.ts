4ec28ae55a0143605ca78464499929f68a003a24
ple <pleknosthe@gmail.com>
2021-09-17 23:38:48 +0300

    make event public

--- backend/src/event/event.controller.ts
+++ backend/src/event/event.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { EventService } from './event.service';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('event')
 export class EventController {
@@ -8,6 +9,7 @@ export class EventController {
   ) {}
 
   @Get()
+  @Public()
   async get(@Req() request) {
     if (request.user.id === 40 || request.user.id === 154 || request.user.id === 24) return { started: true };
 
