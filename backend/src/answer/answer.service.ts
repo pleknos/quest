9162784b5868ce4eabd662a8520c2a77af888468
ple <pleknosthe@gmail.com>
2021-09-18 17:02:41 +0300

    fix

--- backend/src/answer/answer.service.ts
+++ backend/src/answer/answer.service.ts
@@ -10,18 +10,22 @@ export class AnswerService {
   ) {}
 
   async add(answerData: Answer) {
-    const newAnswer = this.answerRepository.create(answerData);
-    const savedAnswer = await this.answerRepository.save(newAnswer);
+    // const newAnswer = this.answerRepository.create(answerData);
+    // const savedAnswer = await this.answerRepository.save(newAnswer);
+    //
+    // if (savedAnswer) {
+    //   return {
+    //     success: true,
+    //   };
+    // } else {
+    //   return {
+    //     success: false,
+    //   };
+    // }
 
-    if (savedAnswer) {
-      return {
-        success: true,
-      };
-    } else {
-      return {
-        success: false,
-      };
-    }
+    return {
+      success: false,
+    };
   }
 
   getWinners() {
@@ -30,14 +34,14 @@ export class AnswerService {
         user_name as name,
         user_email as email,
         user_phone as phone,
-        concat('[ ', group_concat('{ "target": "', target_name, '", "selfie": "', selfie, '" }'), ' ]') AS data,
+        CONCAT('[ ', GROUP_CONCAT('{ "target": "', target_name, '", "selfie": "', selfie, '" }'), ' ]') AS data,
         COUNT(target_name) as count
       FROM (
-          SELECT user.id as user_id, selfie, target.name as target_name, user.name as user_name, user.email as user_email, user.phone as user_phone
-            FROM answer
-            LEFT JOIN user ON user.id = answer.userId
-            LEFT JOIN target ON target.id = answer.targetId
-          ) as allAnswers
+        SELECT user.id as user_id, selfie, target.name as target_name, user.name as user_name, user.email as user_email, user.phone as user_phone
+        FROM answer
+        LEFT JOIN user ON user.id = answer.userId
+        LEFT JOIN target ON target.id = answer.targetId
+      ) as allAnswers
       GROUP BY user_id
       ORDER BY count DESC
       LIMIT 15
